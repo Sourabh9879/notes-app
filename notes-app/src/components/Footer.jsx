48607e49321cf80f3cf45dc372e6ae8e1076b75b
Sourabh <137901668+Sourabh9879@users.noreply.github.com>
2024-06-20 12:07:35 +0530

    initial

---
+++ notes-app/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+const react = require("react");
+
+function Footer() {
+  return <p className="copy">Copyright &copy; 2024 | Sourabh</p>;
+}
+
+export default Footer;
---
+++ notes-app/src/components/Heading.jsx
@@ -0,0 +1,11 @@
+const react = require("react");
+
+function Heading() {
+  return (
+    <header>
+      <h1>Sourabh's Notes</h1>
+    </header>
+  );
+}
+
+export default Heading;
