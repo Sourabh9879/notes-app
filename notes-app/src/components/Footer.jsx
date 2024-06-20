a76766766841ae81c611526e9b00dd901a354b05
Sourabh <137901668+Sourabh9879@users.noreply.github.com>
2024-06-20 12:09:05 +0530

    Delete notes-app directory

--- notes-app/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-const react = require("react");
-
-function Footer() {
-  return <p className="copy">Copyright &copy; 2024 | Sourabh</p>;
-}
-
-export default Footer;
--- notes-app/src/index.jsx
+++
@@ -1,8 +0,0 @@
-const react = require("react");
-const ReactDOM = require("react-dom/client");
-
-import App from './App.jsx'
-
-ReactDOM.createRoot( 
-  document.querySelector('#root')
-).render(<App />)
